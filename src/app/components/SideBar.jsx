b1644524dd2258b312951ed81cff0e149fbaa98d
Naren Sengodan <scartegy@gmail.com>
2025-07-17 03:45:55 +0530

    communities

--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -72,7 +72,7 @@ export default function SideBar() {
     { name: 'Dashboard', icon: Home, href: '/home' },
     { name: 'Explore', icon: Search, href: '/home/explore' },
     { name: 'Fermitor', icon: AtomIcon, href: '/fermitor' },
-    { name: 'Communities', icon: SearchCode, href: '/home/communities' },
+    { name: 'Communities', icon: SearchCode, href: '/communities' },
     { name: 'Messages', icon: MessageSquare, href: '/messages' },
     { name: 'Post', icon: PlusCircle, href: '/home/post' },
   ];
