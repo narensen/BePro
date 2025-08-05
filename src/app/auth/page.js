7f1598afc96b372464ff236579382af7351c48e0
Naren Sengodan <scartegy@gmail.com>
2025-08-05 15:02:44 +0530

    supabase issue rectified

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -2,8 +2,12 @@
 
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import { supabase } from '../lib/supabase_client'
+import { createClient } from '@supabase/supabase-js'
 import useUserStore from '../store/useUserStore'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default function AuthPage() {
   const [isSignIn, setIsSignIn] = useState(true)
