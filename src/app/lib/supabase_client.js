f228756eeb0e54357fa9689dd7959ff86e157a0f
Naren Sengodan <scartegy@gmail.com>
2025-08-05 14:58:23 +0530

    Hopefully

--- src/app/lib/supabase_client.js
+++ src/app/lib/supabase_client.js
@@ -2,5 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export { supabaseKey }
 export const supabase = createClient(supabaseUrl, supabaseKey)
