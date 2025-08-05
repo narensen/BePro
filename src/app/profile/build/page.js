f7f27f31326bcd2c50de4c11a23789796e838ea1
Naren Sengodan <scartegy@gmail.com>
2025-08-05 15:22:31 +0530

    open source

--- src/app/profile/build/page.js
+++ src/app/profile/build/page.js
@@ -2,7 +2,12 @@
 
 import React, { useState, useEffect } from 'react';
 import { Check, X, User, Tag, ArrowRight, Loader2, ChevronLeft, Search, Camera, Upload } from 'lucide-react';
-import { supabase } from '../../lib/supabase_client';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 import { useRouter } from 'next/navigation';
 import { availableTags } from './availableTags';
 
