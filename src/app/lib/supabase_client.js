8df1d3f6bc459635c2d5d136e182e44fa7be3ef6
Naren Sengodan <scartegy@gmail.com>
2025-08-05 14:46:18 +0530

    Open Sourced

--- src/app/lib/supabase_client.js
+++ src/app/lib/supabase_client.js
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export { supabaseKey }
 export const supabase = createClient(supabaseUrl, supabaseKey)
